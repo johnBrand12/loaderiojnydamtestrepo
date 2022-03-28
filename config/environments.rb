0c945d56c83505c4d85eaa3fbe438fde2a86eb98
John Nydam <jnydam@me.com>
2022-03-28 12:49:59 -0400

    Changed config vars

--- config/environments.rb
+++ config/environments.rb
@@ -1,5 +1,5 @@
 configure :production, :development do
-    db = URI.parse(ENV['HEROKU_POSTGRESQL_SILVER_URL'] || 'postgres://localhost:5432')
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost:5432')
 
     ActiveRecord::Base.establish_connection(
         :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
